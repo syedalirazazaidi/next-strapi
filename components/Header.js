4dc03e8f080f402fe8ed7f202602b93c4d84bc82
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-11-24 14:22:59 +0500

    front-end

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 function Header() {
-  return <HeaderStyled>Header</HeaderStyled>;
+  return <HeaderStyled>Header component</HeaderStyled>;
 }
 const HeaderStyled = styled.header`
   background: red;
